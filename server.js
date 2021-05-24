1b74e08f7b90323bba1db5242ec0535c8a574153
Carlos Quesada <carlos-andres-q@hotmail.com>
2021-05-23 23:23:04 -0500

    Proyecto con la creación de mi portafolio

---
+++ server.js
@@ -0,0 +1,43 @@
+let express = require("express"),
+  path = require("path"),
+  nodeMailer = require("nodemailer"),
+  bodyParser = require("body-parser");
+
+let app = express();
+
+app.use(express.static("Mi_Portafolio"));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.post("/send-email", function (req, res) {
+  let transporter = nodeMailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+      // should be replaced with real sender's account
+      user: "thethecoucs@gmail.com",
+      pass: "u2vscoldplay",
+    },
+  });
+  let mailOptions = {
+    // should be replaced with real recipient's account
+    to: "thethecoucs@gmail.com",
+    subject: req.body.subject,
+    body: req.body.message,
+  };
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return console.log(error);
+    }
+    console.log("Message %s sent: %s", info.messageId, info.response);
+  });
+  res.writeHead(301, { Location: "index.html" });
+  res.end();
+});
+
+let server = app.listen(8081, function () {
+  let port = server.address().port;
+  console.log("Server started at http://localhost:%s", port);
+});
